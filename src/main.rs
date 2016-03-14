179ea52bcb598257c643a3d4fd521188f1e6d99f
Can Bülbül <alicanblbl@gmail.com>
2016-03-14 22:08:53 +0200

    error handling & code cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
 	};
 
   let img = match image::open(&Path::new(&path)) {
-	  Result::Err(err) => {
+	  Result::Err(_) => {
 			println!("Error: not an image file!");
 			return;
 		},
@@ -25,22 +25,27 @@ fn main() {
 	};
 
   //get image properties
+	let (width, height) = img.dimensions();
 	let v: Vec<&str> = path.split(".").collect();
 	let w: Vec<&str> = v[0].split("@").collect();
 	let basename = w[0];
-	let scale    = w[1];
-	let (width, height) = img.dimensions();
+	if w.len()<2 {
+		  //create @3x image if not exists
+			println!("Warning: cannot infer scale, assuming @3x.");
+		  let path3x = String::from(basename) + "@3x.png";
+			create_image(path3x, img.clone());
+	}
 
-  //create file references for output files
+  //create image paths
 	let path2x = String::from(basename) + "@2x.png";
 	let path1x = String::from(basename) +    ".png";
-  let ref mut fout2x = File::create(&Path::new(&path2x)).unwrap();
-  let ref mut fout1x = File::create(&Path::new(&path1x)).unwrap();
 
-	let img2x = img.clone().resize(width*2/3, height*2/3,CatmullRom);
-	let img1x = img.clone().resize(width  /3, height  /3,CatmullRom);
-
-  //write images
-  let _ = img2x.save(fout2x, image::PNG).unwrap();
-  let _ = img1x.save(fout1x, image::PNG).unwrap();
+  //create images
+	create_image(path2x, img.clone().resize(width*2/3, height*2/3,CatmullRom));
+	create_image(path1x, img.clone().resize(width*1/3, height*1/3,CatmullRom));
 }
+
+fn create_image(path: String, img: image::DynamicImage) {
+  let ref mut fout = File::create(&Path::new(&path)).unwrap();
+  let _ = img.clone().save(fout, image::PNG).unwrap();
+}	
