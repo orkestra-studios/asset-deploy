6dc33107e2696b3af8e40799e5f1e150646bba64
Can Bülbül <alicanblbl@gmail.com>
2016-03-14 16:09:45 +0200

    v0.1

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+use std::env;
+
+use image::GenericImage;
+use image::FilterType::CatmullRom;
+
+fn main() {
+  let path = &env::args().nth(1).unwrap();
+  let img = image::open(&Path::new(path)).unwrap();
+
+  println!("dimensions {:?}", img.dimensions());
+	let v: Vec<&str> = path.split(".").collect();
+	let w: Vec<&str> = v[0].split("@").collect();
+	let basename = w[0];
+	let scale    = w[1];
+	let (width, height) = img.dimensions();
+
+	println!("file: {} @ {}", basename, scale);
+	let path2x = String::from(basename) + "@2x.png";
+	let path1x = String::from(basename) +    ".png";
+  let img2x = img.clone();
+  let img1x = img.clone();
+
+	img2x.resize(width*2/3, height*2/3,CatmullRom);
+	img1x.resize(width  /3, height  /3,CatmullRom);
+
+  let ref mut fout2x = File::create(&Path::new(&path2x)).unwrap();
+  let ref mut fout1x = File::create(&Path::new(&path1x)).unwrap();
+
+  let _ = img.save(fout2x, image::PNG).unwrap();
+  let _ = img.save(fout1x, image::PNG).unwrap();
+}
