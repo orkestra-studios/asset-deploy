096d56990bf96d4c5b5dc9f97663ad346ec235f8
Can Bülbül <alicanblbl@gmail.com>
2016-03-14 16:13:29 +0200

    output fix

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,13 @@ fn main() {
 	println!("file: {} @ {}", basename, scale);
 	let path2x = String::from(basename) + "@2x.png";
 	let path1x = String::from(basename) +    ".png";
-  let img2x = img.clone();
-  let img1x = img.clone();
 
-	img2x.resize(width*2/3, height*2/3,CatmullRom);
-	img1x.resize(width  /3, height  /3,CatmullRom);
+	let img2x = img.clone().resize(width*2/3, height*2/3,CatmullRom);
+	let img1x = img.clone().resize(width  /3, height  /3,CatmullRom);
 
   let ref mut fout2x = File::create(&Path::new(&path2x)).unwrap();
   let ref mut fout1x = File::create(&Path::new(&path1x)).unwrap();
 
-  let _ = img.save(fout2x, image::PNG).unwrap();
-  let _ = img.save(fout1x, image::PNG).unwrap();
+  let _ = img2x.save(fout2x, image::PNG).unwrap();
+  let _ = img1x.save(fout1x, image::PNG).unwrap();
 }
