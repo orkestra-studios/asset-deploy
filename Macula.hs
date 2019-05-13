e6cf3eb9f04ae64543d69693db2ec7dba2fdb57d
Can Bulbul <alicanblbl@gmail.com>
2019-05-13 11:50:37 +0300

    fixes

--- Macula.hs
+++ Macula.hs
@@ -2,27 +2,35 @@
 import System.Environment
 import System.IO
 
+import Control.Concurrency
+
 import Graphics.Image.Types
 import Graphics.Image.Processing
 
 import qualified Graphics.Image as HIP
 
+type Extension = String
 type IMG = (Image VU RGBA Double) 
+data File = File { 
+  name    :: String, 
+  scale   :: Maybe String,
+  ext     :: String, 
+  content :: IMG
+}
+
+toFile :: String -> IO File
+toFile = undefined
+
+path :: File -> String
+path = undefined
 
 main :: IO ()
 main = do
   args <- getArgs
-  mapM_ (generateImages . getFileProps) args
-  putStrLn "done."
-
-getFileProps :: String -> (String, String, String)
-getFileProps s = (base, scale, ext)
-  where ext = dropWhile (/='.') s
-        base = takeWhile (\c -> c/='@' && c/='.') s
-        scale = takeWhile (/='.') $ dropWhile (/='@') s
-
-makeFileName :: (String, String, String) -> String
-makeFileName (b,s,e) = b ++ s ++ e
+  if null args then
+    putStrLn "nothing to do."
+  else
+    mapM_ (generateImages . getFileProps) args
 
 generate1x :: (String, String) -> IMG -> IO()
 generate1x (b,e) img = do
@@ -42,12 +50,25 @@ generate2x (b,e) img = do
   putStrLn "done."
   where filename = makeFileName (b,"@2x",e)
 
+resizeTo :: (Base, Extension) -> Int -> IMG -> IMG
+resizeTo (b,e) d = do
+  putStr $ "  generating " ++ filename ++ "... "
+  hFlush stdout
+  HIP.writeImage filename (scale Bilinear Edge (d/1024.0, d/1024.0))
+  putStrLn "done."
+  where filename = b++"-"++d++"px."++e 
+
 generateImages :: (String, String, String) -> IO()
 generateImages (b, "@3x", e) = do
   putStrLn $ "convert: " ++ filename
   img <- HIP.readImageRGBA VU filename
-  generate2x (b,e) img
-  generate1x (b,e) img
+  forkIO $ generate2x (b,e) img
+  forkIO $ generate1x (b,e) img
   putStrLn ""
   where filename = makeFileName (b,"@3x",e)
+generateImages (b, "@base", e) = do
+  putStrLn $ "generating " ++ filename ++ ".Appicon"
+  img <- HIP.readImageRGBA VU filename
+  mapM_ (\d -> forkIO $ resizeTo (b,e) d img) [29,40,57,58,60,80,120,114,180]
+  where filename = makeFileName (b,"@base",e)
 generateImages (b, s, e) = putStrLn $ "ignore: " ++ b ++ s ++ e ++ "\n"
